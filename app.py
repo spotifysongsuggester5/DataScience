7701a38f22f1a7ab5e3e66c1f3c5b959ef54aad8
rselent <rselent@ufl.edu>
2020-02-04 14:32:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,42 @@
 	based on acoustic similarities
 """
 
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template
+from model import Song, User
 
-APP = Flask( __name__)
-
-
-@APP.rout( '/')
-
-
+app = Flask( __name__)
+app.config[ 'SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+DB.init_app( app)
 
 
+@APP.route( '/', methods= ['POST'])
+def root():
+	DB.drop_all()
+	DB.create_all()
 
+	# < Potential For loop here > (if multiple trackIDs will be passed in a list)
+	track_id = request.values[ 'track_id']
+
+	DB.session.add( track_id)
+	DB.commit()
+
+
+def suggestSong():
+	pass
+
+
+def exportSuggestion():
+	pass
+
+
+
+"""
+populate Song table with csv
+user > inputs( track_id)    (^ up there)
+add that User.track_id to db > compare to Song.track_id to get all other fields
+run model to find similar songs
+export new track_ids OR Song.songName and Song.artistName
+
+
+"""
