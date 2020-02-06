9f62ca50af45fa581b98886d5e839206842b5902
Ro McKay Davies <50719703+120Davies@users.noreply.github.com>
2020-02-06 12:06:25 -0700

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -3,22 +3,36 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import NearestNeighbors
 from sklearn.model_selection import train_test_split
-sns.set()
+from sklearn import preprocessing
+import pickle
 
-df = pd.read_csv('SpotifyAudioFeaturesApril2019.csv')
+df = pd.read_csv('spotify2019.csv')
 
 target = 'track_id'
-features = df.columns.drop(['track_id', 'artist_name', 'track_name'])
+features = df.columns.drop(['track_id', 'artist_name', 'track_name', 'key', 'mode', 'time_signature' 'duration_ms'])
 
-X = X[features]
-y = df[target]
+X = df[features].values
+y = df[target].values
 
-knn = KNeighborsClassifier(n_neighbors=10, metric='euclidean')
-knn.fit(X_train, y_train)
+#Value normalization and dimensionality reduction
+pca = PCA(n_components=2)
+X = StandardScaler().fit_transform(X)
+principalComponents = pca.fit_transform(X)
+X = pd.DataFrame(data = principalComponents
+                           , columns = ['x', 'y'])
 
-y = knn.kneighbors([X_train.iloc[30]])[1]
+knn = KNeighborsClassifier(n_neighbors=10)
+knn.fit(X, y)
+
+y = knn.kneighbors([X.iloc[25173]])[1]
 y = y.tolist()
 y
 
 for i in y:
     df['track_id'].loc[i]
+
+for i in y:
+
+# # Pickle the model
+# filename = 'knn'
+# pickle.dump(knn, open(filename, 'wb'))
